dc07900a5587fa8d6024a4facee0bcc229eeb3c1
anuraagpatil10 <anuraagpatil10@gmail.com>
2025-12-27 15:14:28 +0530

    minor changes

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,12 +1,16 @@
-import Head from 'next/head';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
-        <Head>
-            <title>GearGuard - Maintenance Management System</title>
-            <meta name="description" content="Professional maintenance management system" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <Html lang="en">
+            <Head>
+                <meta name="description" content="Professional maintenance management system" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
     );
 }
